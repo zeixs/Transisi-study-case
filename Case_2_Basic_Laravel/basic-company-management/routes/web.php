8036a734727442b59d77710170de93bf085de56b
zeixs <ur.syid07@gmail.com>
2023-02-15 00:05:31 +0700

    Add Controller

--- Case_2_Basic_Laravel/basic-company-management/routes/web.php
+++ Case_2_Basic_Laravel/basic-company-management/routes/web.php
@@ -21,6 +21,5 @@ Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('/company', 'CompanyController');
+Route::resource('/employee', 'EmployeeController');
